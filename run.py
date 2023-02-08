a046651cd5f0d943f6364af4c00ff723754fddaf
bethanyalicehall <bethany.hall66@gmail.com>
2023-02-08 15:18:20 +0000

    Added registration page and form within that, also added werkzeug security imports within run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_review():
     reviews = mongo.db.review.find()
     return render_template("review.html", reviews=reviews)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 # change debug to false below!
 
