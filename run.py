adb35942f21f9188f7591577f32f4f34f191bfd0
bethanyalicehall <bethany.hall66@gmail.com>
2023-02-09 10:28:38 +0000

    Added login functionality, and link to login page from the register page

--- run.py
+++ run.py
@@ -47,6 +47,33 @@ def register():
         flash("Registration Successful!")
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check username is already registered and stored within database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check hashed password match password input from user
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # when password is incorrect/does not match hashed password
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist within the database
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 # change debug to false below!
 
 
